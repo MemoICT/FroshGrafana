af7166b5b43d772e7bf58acf80ffd9300f998762
Kai Neuwerth <kai.neuwerth@netshops.de>
2018-02-28 10:17:50 +0100

    Fixes zipping of repository content on travis build

--- build.sh
+++ build.sh
@@ -2,12 +2,14 @@
 
 commit=$1
 if [ -z ${commit} ]; then
-    commit=$(git tag | tail -n 1)
+    commit=$TRAVIS_COMMIT
     if [ -z ${commit} ]; then
         commit="master";
     fi
 fi
 
+echo "Packing commit/release: ${commit}"
+
 # Build new release
 mkdir -p FroshGrafana
-git archive --format zip --output FroshGrafana/FroshGrafana-${commit}.zip $1
+git archive HEAD --format zip --output FroshGrafana/FroshGrafana-${commit}.zip
